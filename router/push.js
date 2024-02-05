c4483eab86a12203a23e303f550edf9cad6cb07e
SSkkky <worte5633@gmail.com>
2024-02-05 10:10:11 +0900

    UPDATE

--- router/push.js
+++ router/push.js
@@ -10,28 +10,28 @@ webpush.setVapidDetails(
 );
 
 pushRouter.get('/', async function (req, res) {
-    res.send('push ready');
+    res.send('push ready...');
 })
 
-pushRouter.get('/publicKey', async function (req, res) { // 퍼블릭키는 프론트단에 보내야함
-    // res.send(vapidKeys.publicKey);
-    res.send(vapidKeys.publicKey);
+pushRouter.get('/publicKey', function (req, res) {
+  res.send(vapidKeys.publicKey)
 })
+
 pushRouter.post('/sendNoti', function (req, res) {
 
-    let data = JSON.stringify({msg:'hello pwa'})
-  
-    setTimeout(function () {
-        webpush.sendNotification(req.body.subscribe,data)
-        .then(function () {
-          res.sendStatus(202);
-        })
-        .catch(function (error) {
-            res.sendStatus(500);
-            console.log(error);
-        });
-    }, 3000);
-  
-  })
+  let data = JSON.stringify({msg:'hello pwa'})
+
+  setTimeout(function () {
+      webpush.sendNotification(req.body.subscribe,data)
+      .then(function () {
+        res.sendStatus(202);
+      })
+      .catch(function (error) {
+          res.sendStatus(500);
+          console.log(error);
+      });
+  }, 3000);
+
+})
 
 module.exports = pushRouter;
