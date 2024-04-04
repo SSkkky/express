4c18e388a1db470babdf0f29fce4b618f8f18878
SSkkky <worte5633@gmail.com>
2024-04-04 09:44:06 +0900

    add mysql

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 const cors = require('cors');
 const bucket = require('./router/bucket.js');
 const push = require('./router/push.js');
+const vue = require('./router/vue.js');
 const app = express()
 
 app.use(cors())
@@ -12,6 +13,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use('/', bucket)
 app.use('/push', push)
+app.use('/vue', vue)
 
 const PORT = 3333;
 app.listen(PORT, () => {
